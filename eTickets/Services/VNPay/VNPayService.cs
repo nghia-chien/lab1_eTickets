d5929062fa0c74f160f172783bf97453fe8a0307
hacphichien <hacphichien@gmail.com>
2025-02-28 09:50:29 +0700

    Nghia

---
+++ eTickets/Services/VNPay/VNPayService.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Globalization;
+using eTickets.Libraries;
+using eTickets.Models.VNPay;
+
+namespace eTickets.Services.VNPay
+{
+    public class VNPayService : IVnPayService
+    {
+        private readonly IConfiguration _configuration;
+
+        public VNPayService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string CreatePaymentUrl(PaymentInformationModel model, HttpContext context)
+        {
+            var timeZoneId = _configuration["TimeZoneId"] ?? "Asia/Ho_Chi_Minh";
+            var timeZone = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+            var timeNow = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, timeZone);
+            var tick = DateTime.Now.Ticks.ToString();
+
+            var pay = new VNPayLibrary();
+            var urlCallBack = _configuration["PaymentCallBack:ReturnUrl"];
+
+            // Thêm thông tin quan trọng
+            pay.AddRequestData("vnp_Version", "2.1.0");
+            pay.AddRequestData("vnp_Command", "pay");
+            pay.AddRequestData("vnp_TmnCode", _configuration["Vnpay:TmnCode"]);
+            pay.AddRequestData("vnp_Amount", (model.Amount * 100).ToString()); // Chuyển amount thành VNPay format
+            pay.AddRequestData("vnp_CreateDate", timeNow.ToString("yyyyMMddHHmmss"));
+            pay.AddRequestData("vnp_CurrCode", "VND");
+            pay.AddRequestData("vnp_IpAddr", pay.GetIpAddress(context));
+            pay.AddRequestData("vnp_Locale", "vn");
+            pay.AddRequestData("vnp_OrderInfo", $"{model.Name} - {model.OrderDescription} - {model.Amount} VND");
+            pay.AddRequestData("vnp_OrderType", model.OrderType);
+            pay.AddRequestData("vnp_ReturnUrl", urlCallBack);
+            pay.AddRequestData("vnp_TxnRef", tick);
+
+            // Kiểm tra có truyền bankCode không
+            if (!string.IsNullOrEmpty(_configuration["Vnpay:BankCode"]))
+            {
+                pay.AddRequestData("vnp_BankCode", _configuration["Vnpay:BankCode"]);
+            }
+
+            var paymentUrl = pay.CreateRequestUrl(_configuration["Vnpay:BaseUrl"], _configuration["Vnpay:HashSecret"]);
+            Console.WriteLine("VNPay URL: " + paymentUrl);
+
+
+            //var paymentUrl = pay.CreateRequestUrl(_configuration["Vnpay:BaseUrl"], _configuration["Vnpay:HashSecret"]);
+            return paymentUrl;
+        }
+
+        public PaymentResponseModel PaymentExecute(IQueryCollection collections)
+        {
+            var pay = new VNPayLibrary();
+            var response = pay.GetFullResponseData(collections, _configuration["Vnpay:HashSecret"]);
+            return response;
+        }
+    }
+}
