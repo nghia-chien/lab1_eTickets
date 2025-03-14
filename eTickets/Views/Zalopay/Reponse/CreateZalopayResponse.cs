de756a0348d6f21c8d334b1d78906aac99927321
hacphichien <hacphichien@gmail.com>
2025-03-14 20:48:57 +0700

    Cập nhật cấu hình và thêm view Zalopay

---
+++ eTickets/Views/Zalopay/Reponse/CreateZalopayResponse.cs
@@ -0,0 +1,30 @@
+﻿namespace eTickets.Views.Zalopay.Reponse
+{
+    public class CreateZalopayResponse
+    {
+        public CreateZalopayResponse(int appId, string appUser, long appTime, string appTransId, string description, string bankCode, long amount)
+        {
+            AppId = appId;
+            AppUser = appUser;
+            AppTime = appTime;
+            AppTransId = appTransId;
+            Description = description;
+            BankCode = bankCode;
+            Amount = amount;
+        }
+        public int AppId { get; set; }
+        public string AppUser { get; set; } = string.Empty;
+        public long AppTime { get; set; }
+        public long Amount { get; set; }
+        public string AppTransId { get; set; } = string.Empty;
+        public string ReturnUrl { get; set; }
+        public string EmbedData { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string BankCode { get; set; } = string.Empty;
+        public string Mac { get; set; } = string.Empty;
+        public void MakeSingature(string key)
+        {
+            // Make payment
+        }
+    }
+}
---
+++ eTickets/Views/Zalopay/Request/CreateZalopayPayRequest.cs
@@ -0,0 +1,27 @@
+﻿namespace eTickets.Views.Zalopay.Request
+{
+    public class CreateZalopayPayRequest
+    {
+        public CreateZalopayPayRequest(int appId ,string appUser, long appTime, string appTransId, string description, string bankCode, long amount)
+        {
+            AppId = appId;
+            AppUser = appUser;
+            AppTime = appTime;
+            AppTransId = appTransId;
+            Description = description;
+            BankCode = bankCode;
+            Amount = amount;
+        }
+        public int AppId { get; set; }
+        public string AppUser { get; set; } = string.Empty;
+        public long AppTime { get; set; }
+        public long Amount { get; set; }
+        public string AppTransId { get; set; } = string.Empty;
+        public string ReturnUrl { get; set; }
+        public string EmbedData { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string BankCode { get; set; } = string.Empty;
+        public string Mac { get; set; } = string.Empty;
+        
+    }
+}
